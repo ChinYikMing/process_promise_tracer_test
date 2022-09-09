13f45a615c56c5ec37c1950601f1a0a23f6e5968
OEM Configuration (temporary user) <a82951706@gmail.com>
2022-09-09 15:29:09 +0800

    Add ptrace_test folder.

---
+++ ptrace_test/test.h
@@ -0,0 +1,27 @@
+#include <sys/reg.h>
+#include <sys/ptrace.h>
+#include <sys/user.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <errno.h>
+#include <sys/mman.h>
+#include <sys/ptrace.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <assert.h>
+#include <sys/mman.h>
+#include <string.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdint.h>
+
+void call_mmap();
